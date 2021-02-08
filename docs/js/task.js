d965027ad2e8555c1fb88daf1d0ea278755271f5
Alex Viktorov <viktorovaleksandr7@gmail.com>
2021-02-08 23:03:24 +0200

    add delete foo

--- docs/js/task.js
+++ docs/js/task.js
@@ -1,59 +1,55 @@
-function renderAlbums(todoTitle,id) {
-	const ulTodo = document.querySelector('.js-list-todo');
-	const list = document.createElement('div');
-	list.className = `alert alert-dark d-flex 
-	justify-content-between`;
+const ulTodo = document.querySelector('.js-list-albums');
+const wrapper = document.querySelector('.js-gallery-photo');
 
-	list.dataset.id = id;
-	list.textContent = todoTitle;
-	ulTodo.append(list);
+function renderAlbums(title,id) {
+	const albumLists = document.createElement('div');
+	albumLists.className = `alert alert-warning`;
+	albumLists.dataset.id = id;
+	albumLists.textContent = title;
+	ulTodo.append(albumLists);
 }
-function eventClickAlbum(todo) {
-	const ulTodo = document.querySelector('.js-list-todo');
-
+function onClickAlbumsItem() {
   	ulTodo.addEventListener('click', (event) => {
-		const album = event.target.closest('div');
-		const albumId = album.dataset.id;
+		const albumItem = event.target.closest('div');
+		const albumId = albumItem.dataset.id;
 
-		getNewFetch(albumId);
+		sendGetPhotosRequest(albumId);
 	});	
 }
-function getNewFetch(albumId) {
+function sendGetPhotosRequest(albumId) {
 	fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
 	.then((response) => response.json())
 	.then((todos) => {
-		todos.map((todo) => {
-			renderPhotos(todo);
+		wrapper.innerHTML = '';		
+		todos.map((photo) => {
+			renderPhotos(photo);
    	});
 	})
+	.catch((error) => console.error('Ошибка:', error));
 }
-eventClickAlbum();
-
-function renderPhotos(todo) {
-	const div = document.querySelector('.col-md-4');
+function renderPhotos(photo) {
 	const img = document.createElement('img');
-
-	img.src = todo.url;
-   img.width = 550;
-   img.height = 150;
-   div.append(img);
+	img.src = photo.url;
+  	img.width = 550;
+  	img.height = 150;
+  	wrapper.append(img);
 }
+onClickAlbumsItem();
+
 fetch('https://jsonplaceholder.typicode.com/albums')
-
 	.then((response) => response.json())
-	.then((user) => {
-		user.map((todo,id) => {
-   		renderAlbums(todo.title,id);	
-   	});
-   	return fetch('https://jsonplaceholder.typicode.com/photos?albumId=1')
-	})
-	.then((response) => response.json())
-	.then((todos) => {
-		todos.map((todo) => {
-			renderPhotos(todo);
+	.then((albums) => {
+		albums.map((item,id) => {
+   		renderAlbums(item.title,id);	
    	});
 	})
-	.catch((error) => console.log('Error'));
+	.catch((error) => console.error('Ошибка:', error));
 
-// 1) удолять предыдущие фото
-	// img.remove();
+fetch('https://jsonplaceholder.typicode.com/photos?albumId=1')
+	.then((response) => response.json())
+	.then((albums) => {
+		albums.map((albumItem) => {
+			renderPhotos(albumItem);
+   	});
+	})
+	.catch((error) => console.error('Ошибка:', error));
