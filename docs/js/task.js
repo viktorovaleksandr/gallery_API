9f4f9a03f716afc963c82ed71616aa15c6b09eb4
Alex Viktorov <viktorovaleksandr7@gmail.com>
2021-02-09 22:36:43 +0200

    add foo

--- docs/js/task.js
+++ docs/js/task.js
@@ -1,33 +1,6 @@
 const ulTodo = document.querySelector('.js-list-albums');
 const wrapper = document.querySelector('.js-gallery-photo');
 
-// Event Listener
-function onClickAlbumsItem() {
-  	ulTodo.addEventListener('click', (event) => {
-		const albumItem = event.target.closest('div');
-		const albumId = albumItem.dataset.id;
-
-		sendGetPhotosRequest(albumId);
-	});	
-}
-onClickAlbumsItem();
-
-// Render 
-function renderAlbums(title,id) {
-	const albumLists = document.createElement('div');
-	albumLists.className = `alert alert-warning`;
-	albumLists.dataset.id = id + 1;
-	albumLists.textContent = title;
-	ulTodo.append(albumLists);
-}
-function renderPhotos(photo) {
-	const img = document.createElement('img');
-	img.src = photo.url;
-  	img.width = 550;
-  	img.height = 150;
-  	wrapper.append(img);
-}
-
 // Request
 function sendGetPhotosRequest(albumId) {
 	fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
@@ -63,3 +36,30 @@ function sendGetPhotoRequest() {
 	.catch((error) => console.error('Ошибка:', error));
 }
 sendGetPhotoRequest()
+
+// Render 
+function renderAlbums(title,id) {
+	const albumLists = document.createElement('div');
+	albumLists.className = `alert alert-warning`;
+	albumLists.dataset.id = id + 1;
+	albumLists.textContent = title;
+	ulTodo.append(albumLists);
+}
+function renderPhotos(photo) {
+	const img = document.createElement('img');
+	img.src = photo.url;
+  	img.width = 550;
+  	img.height = 150;
+  	wrapper.append(img);
+}
+
+// Event Listener
+function onClickAlbumsItem() {
+  	ulTodo.addEventListener('click', (event) => {
+		const albumItem = event.target.closest('div');
+		const albumId = albumItem.dataset.id;
+
+		sendGetPhotosRequest(albumId);
+	});	
+}
+onClickAlbumsItem();
