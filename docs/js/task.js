ad9e160c5c190b7f492fc0c624886f7dc4909d8c
Alex Viktorov <viktorovaleksandr7@gmail.com>
2021-02-09 22:33:22 +0200

    add foo

--- docs/js/task.js
+++ docs/js/task.js
@@ -1,13 +1,7 @@
 const ulTodo = document.querySelector('.js-list-albums');
 const wrapper = document.querySelector('.js-gallery-photo');
 
-function renderAlbums(title,id) {
-	const albumLists = document.createElement('div');
-	albumLists.className = `alert alert-warning`;
-	albumLists.dataset.id = id;
-	albumLists.textContent = title;
-	ulTodo.append(albumLists);
-}
+// Event Listener
 function onClickAlbumsItem() {
   	ulTodo.addEventListener('click', (event) => {
 		const albumItem = event.target.closest('div');
@@ -16,6 +10,25 @@ function onClickAlbumsItem() {
 		sendGetPhotosRequest(albumId);
 	});	
 }
+onClickAlbumsItem();
+
+// Render 
+function renderAlbums(title,id) {
+	const albumLists = document.createElement('div');
+	albumLists.className = `alert alert-warning`;
+	albumLists.dataset.id = id + 1;
+	albumLists.textContent = title;
+	ulTodo.append(albumLists);
+}
+function renderPhotos(photo) {
+	const img = document.createElement('img');
+	img.src = photo.url;
+  	img.width = 550;
+  	img.height = 150;
+  	wrapper.append(img);
+}
+
+// Request
 function sendGetPhotosRequest(albumId) {
 	fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
 	.then((response) => response.json())
@@ -27,29 +40,26 @@ function sendGetPhotosRequest(albumId) {
 	})
 	.catch((error) => console.error('Ошибка:', error));
 }
-function renderPhotos(photo) {
-	const img = document.createElement('img');
-	img.src = photo.url;
-  	img.width = 550;
-  	img.height = 150;
-  	wrapper.append(img);
-}
-onClickAlbumsItem();
-
-fetch('https://jsonplaceholder.typicode.com/albums')
-	.then((response) => response.json())
-	.then((albums) => {
+function sendGetAlbumsRequest() {
+	return fetch('https://jsonplaceholder.typicode.com/albums')
+		.then((response) => response.json())
+		.then((albums) => {
 		albums.map((item,id) => {
    		renderAlbums(item.title,id);	
    	});
 	})
 	.catch((error) => console.error('Ошибка:', error));
+}
+sendGetAlbumsRequest();
 
-fetch('https://jsonplaceholder.typicode.com/photos?albumId=1')
-	.then((response) => response.json())
-	.then((albums) => {
+function sendGetPhotoRequest() {
+	return fetch('https://jsonplaceholder.typicode.com/photos?albumId=1')
+		.then((response) => response.json())
+		.then((albums) => {
 		albums.map((albumItem) => {
 			renderPhotos(albumItem);
    	});
 	})
 	.catch((error) => console.error('Ошибка:', error));
+}
+sendGetPhotoRequest()
