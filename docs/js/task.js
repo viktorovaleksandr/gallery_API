3a3a0cd5e341ffd33dba2b1c3ffe31389a9859ec
Alex Viktorov <viktorovaleksandr7@gmail.com>
2021-02-16 00:01:01 +0200

    fix

--- docs/js/task.js
+++ docs/js/task.js
@@ -1,77 +1,61 @@
-class GalleryRequests {
-	static sendGetAlbumsRequest() {
-		return fetch('https://jsonplaceholder.typicode.com/albums').then((response) => response.json())
-			
-			.then((albums) => {
-				albums.map((albumList,id) => {
-	   			getAlbums(albumList,id);	
-	   	});
-		})
-	}
+const containerGalleryAlbums = document.querySelector('.js-gallery-albums');
+const containerGalleryPhotos = document.querySelector('.js-gallery-photo');
 
-	static sendGetFirstAlbumPhotosRequest() {
-		return fetch('https://jsonplaceholder.typicode.com/photos?albumId=1').then((response) => response.json())
-			
-			.then((albums) => {
-				renderPhotos(albums);
-			return getPhotos();
-		})
-	}
+// Request
 
-	static sendGetPhotosRequest(albumId) {
-		return fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`).then((response) => response.json())
-	}
+function sendGetAlbumsRequest() {
+	return fetch('https://jsonplaceholder.typicode.com/albums').then((response) => response.json())
+		.then((albums) => {
+			renderAlbums(albums);
+	   	sendGetPhotosRequest(albums[0].id);
+	})
 }
 
-function getAlbums(albumList,albumId) {
-	const divElement = document.createElement('div');
-	divElement.className = `alert alert-warning`;
-	divElement.dataset.id = albumId + 1;
-	divElement.textContent = albumList.title;
-	containerGalleryAlbums.append(divElement);
-}
-
-function getPhotos(event) {
-	const albumItem = event.target.closest('div');
-	const albumId = albumItem.dataset.id;
-
-	const promise = GalleryRequests.sendGetPhotosRequest(albumId);
-
-	promise.then((albums) => {
+function sendGetPhotosRequest(albumId) {
+	return fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`).then((response) => response.json())
+	.then((albums) => {
 		containerGalleryPhotos.innerHTML = '';		
 		renderPhotos(albums);
 	})
 }
 
+// RENDER 
+
+function renderAlbums(albums) {
+	albums.map((albumList,id) => {
+	   const divElement = document.createElement('div');
+		divElement.className = `alert alert-warning`;
+		divElement.dataset.id = id + 1;
+		divElement.textContent = albumList.title;
+		containerGalleryAlbums.append(divElement);	
+	});
+}
+
 function renderPhotos(albums) {
 	albums.map((image) => {
-   	createImage(image);	
+   	const img = document.createElement('img');
+		img.src = image.url;
+  		img.width = 550;
+  		img.height = 150;
+  		containerGalleryPhotos.append(img);	
    });
 }
 
-const containerGalleryAlbums = document.querySelector('.js-gallery-albums');
-const containerGalleryPhotos = document.querySelector('.js-gallery-photo');
+// EVENT LISTENERS
+
+function createPhotosEventListener() {
+	containerGalleryAlbums.addEventListener('click', (event) => {
+		const albumItem = event.target.closest('div');
+		const albumId = albumItem.dataset.id;
+		sendGetPhotosRequest(albumId);
+	});	
+}
+
+// INIT 
 
 init();
 
 function	init() {
-	GalleryRequests.sendGetAlbumsRequest();
-	GalleryRequests.sendGetFirstAlbumPhotosRequest();
-	createPhotosListener();
+	sendGetAlbumsRequest();
+	createPhotosEventListener();
 }
-
-function createPhotosListener() {
-  	containerGalleryAlbums.addEventListener('click', (event) => {
-		getPhotos(event);
-	});	
-}
-
-function createImage(image) {
-	const img = document.createElement('img');
-	img.src = image.url;
-  	img.width = 550;
-  	img.height = 150;
-  	containerGalleryPhotos.append(img);
-}
-
-
